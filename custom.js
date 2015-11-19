db3ae7956a1f94202b6e6428838bbd30c494ccb0
Michal Štěpánek <discursive.democracy@gmail.com>
2015-11-19 11:11:30 +0000

    h1 inverts colours

--- custom.js
+++ custom.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $( "p:last-child" ).addClass( "xx-small" );
-    $("p").click(function(){
-        $("p:hover").toggleClass("xx-small");
+    $("h1,p").click(function(){
+        $("p:hover,h1:hover").toggleClass("xx-small");
+        });
+   $("h1").click(function(){
+        $("body").toggleClass("inverted");
         });
 });
