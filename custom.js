50a4479f2629b067816e92606bf442f88fcbea2a
Michal Štěpánek <discursive.democracy@gmail.com>
2015-11-19 23:03:13 +0000

    paragraphs overflows hidding

--- custom.js
+++ custom.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-    $( "p:nth-child(n+3)" ).addClass( "xx-small" );
-    $("h1,p").click(function(){
-        $("p:hover,h1:hover").toggleClass("xx-small");
+    $( "p" ).addClass( "hidden" );
+    $( "p" ).click(function(){
+	$( this ).toggleClass("hidden");
+        });
+    $("h1").click(function(){
+        $("h1").toggleClass("xx-small");
         });
    $("h1").click(function(){
         $("body").toggleClass("inverted");
