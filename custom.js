f6bd036609feeee3fc35f1516b2cd698fb0c795e
Michal Štěpánek <discursive.democracy@gmail.com>
2015-11-20 16:26:40 +0000

    changed fist paragraph texts

--- custom.js
+++ custom.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
-    $( "p" ).addClass( "hidden" );
+    $( "p, body > div" ).addClass( "hidden" );
     $( "p" ).click(function(){
 	$( this ).toggleClass("hidden");
+        $( this ).parent().removeClass("hidden");
+        $( this ).parent().parent().removeClass("hidden");
         });
+    $( "body > p" ).click(function(){
+	$( this ).next().toggleClass("hidden");
+        });5
+   
+
     $("h1").click(function(){
         $("h1").toggleClass("xx-small");
-        });
-   $("h1").click(function(){
         $("body").toggleClass("inverted");
         });
 });
